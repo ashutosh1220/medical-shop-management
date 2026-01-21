cefc5a1661b6bd0bdc97a15309dd2a6b04e5a442
Ashutosh Vishwakarma <150835154+ashutosh1220@users.noreply.github.com>
2026-01-21 18:39:32 +0530

    Updated at 21/1/2026

---
+++ MedicalShopManagement/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+/// <summary>
+/// Summary description for DBConnection
+/// </summary>
+public class DBConnection
+{
+    public DBConnection()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public SqlConnection GetConnection()
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.; initial catalog=MedicalShopDB; Integrated Security=True");
+        return con;
+    }
+
+}
---
+++ MedicalShopManagement/DBManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+/// <summary>
+/// Summary description for DBManager
+/// </summary>
+public class DBManager
+{
+    public DBManager()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    DBConnection conn = new DBConnection();
+    public void EXESQL(string str)
+    {
+        string msg = string.Empty;
+        SqlConnection con = conn.GetConnection();
+        SqlCommand cmd = new SqlCommand(str, con);
+        con.Open();
+        int IsTrue = cmd.ExecuteNonQuery();
+        con.Close();
+        if (IsTrue == 1)
+        {
+            msg = "Successfull inserted";
+        }
+        else
+        {
+            msg = "Error occured!Please Try Again.";
+        }
+
+    }
+    public DataTable GetData(string str)
+    {
+        string msg = string.Empty;
+        SqlConnection con = conn.GetConnection();
+        SqlDataAdapter adp = new SqlDataAdapter(str, con);
+        DataTable dt = new DataTable();
+        int IsTrue = adp.Fill(dt);
+        if (IsTrue == 1)
+        {
+            msg = "Successfully Retrive Data";
+        }
+        else
+        {
+            msg = "Error occured! please Try Again.";
+        }
+        return dt;
+    }
+}
+        
+  
